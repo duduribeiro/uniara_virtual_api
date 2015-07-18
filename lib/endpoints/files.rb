19635d1dfc3e42f93214ccf384ebf3d8da8f8622
Carlos Ribeiro <duduribeiro.gba@gmail.com>
2015-07-18 16:06:49 -0300

    Replace the link file to use the API endpoint

--- lib/endpoints/files.rb
+++ lib/endpoints/files.rb
@@ -1,11 +1,32 @@
+require 'pry'
 module Endpoints
   class Files < Base
     namespace '/files' do
 
       get do
-        encode UniaraVirtualParser.files token
+        json = replace_files_link(UniaraVirtualParser.files token)
+        encode json
       end
 
+      get '/:id' do
+      end
+
+    end
+
+    private
+
+    def replace_files_link(json)
+      new_json = {}
+      json.map do |k,v|
+        new_json[k] = v.map do |file|
+          UniaraVirtualParser::Models::File.new(
+            name: file.name,
+            link: file.link.gsub(/\/alunos\/consultas\/arquivos\//, '/files/'),
+            grade: file.grade
+          )
+        end
+      end
+      new_json
     end
   end
 end
