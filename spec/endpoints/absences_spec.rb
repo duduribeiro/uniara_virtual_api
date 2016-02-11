d87f2aaf29b3944bdf0781349686e04c49455ae0
Carlos Eduardo Ribeiro <mail@carlosribeiro.me>
2016-02-11 20:24:43 -0300

    Fix indentation

--- spec/endpoints/absences_spec.rb
+++ spec/endpoints/absences_spec.rb
@@ -3,20 +3,19 @@ require "spec_helper"
 describe Endpoints::Absences do
   include Rack::Test::Methods
 
-    describe "GET /absences", :vcr do
-
-      context 'with a logged user' do
-        it 'fetch all the absences' do
-          get '/absences', nil, { 'HTTP_AUTHORIZATION' => 'edtht622j7o4aknragshbqdik6' }
-            assert_equal 0, JSON.parse(last_response.body).length
-        end
+  describe "GET /absences", :vcr do
+    context 'with a logged user' do
+      it 'fetch all the absences' do
+        get '/absences', nil, { 'HTTP_AUTHORIZATION' => 'edtht622j7o4aknragshbqdik6' }
+        assert_equal 0, JSON.parse(last_response.body).length
       end
+    end
 
-      context 'with a not logged user' do
-        it 'brings an empty array' do
-          get '/absences', nil, { 'HTTP_AUTHORIZATION' => 'invalidtoken' }
-          assert_equal 0, JSON.parse(last_response.body).length
-        end
-     end
+    context 'with a not logged user' do
+      it 'brings an empty array' do
+        get '/absences', nil, { 'HTTP_AUTHORIZATION' => 'invalidtoken' }
+        assert_equal 0, JSON.parse(last_response.body).length
+      end
+    end
   end
 end
